e52d112e0bfec94554fe5ef99f3b4c050b5ecd9b
eduardaarosaa <eduardacirina@gmail.com>
2019-06-26 15:21:06 -0300

    Body parser

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize("sistemaDeCadastro","root","",{
     host:"localhost",
     dialect:'mysql'
 })
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 //template handlebars
 
@@ -20,7 +23,9 @@ app.get('/card', function(req,res){
     res.render('layouts/form');
 })
 app.post('/add',function(req,res){
-    res.send("formulário recebido");
+
+    res.send("Texto:" + req.body.titulo+"Conteudo:"+req.body.conteudo);
+
 })
 
 
